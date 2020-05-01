a51ca4f7a059927a1021193ec8b49befa971dac2
Alejandro <alex.agc@outlook.com>
2020-05-01 03:36:19 -0500

    Replaced DeviceEventData for Rtl433Message for deserialization

---
+++ Specter.EventProcessing/Events/Rtl433Message.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Specter.EventProcessing.Events
+{
+    public class Rtl433Message
+    {
+        [JsonIgnore]
+        public DateTimeOffset Time { get; } = DateTimeOffset.UtcNow;
+
+        public string Model { get; set; }
+
+        public int Id { get; set; }
+
+        public int Channel { get; set; }
+
+        public int Event { get; set; }
+
+        public string State { get; set; }
+
+        public int Heartbeat { get; set; }
+    }
+}
